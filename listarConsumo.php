ba6b7b3cfe15cf8c5bc9790b93677d9bfe80320c
SGRH <mailsoninfo@gmail.com>
2016-09-10 16:48:35 -0300

    Atualizado 10-09
    Calcula de consumo

--- listarConsumo.php
+++ listarConsumo.php
@@ -8,16 +8,37 @@ require_once 'DB.php';
 	$stmt = DB::prepare($sql);
 	$stmt->bindParam(1, $_GET["idEnd"], PDO::PARAM_INT);
 	$stmt->execute();
-	
-	$result_json = array();
+	$sel_row = $stmt->fetch(PDO::FETCH_ASSOC);
+	$volume_consumido = (double) $sel_row['soma'];
 
-	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-		$result_json[] = $row;
+	$volume_cubico_consumido = $volume_consumido * 0.001;
 
-	}
-	
+	$sql = "SELECT 
+				cast(sum(
+					if (idfaixa = 1, valor_agua, 
+						if ($volume_cubico_consumido>faixa_final, ((faixa_final-faixa_inicial+1)*valor_agua), (($volume_cubico_consumido-faixa_inicial+1)*valor_agua))
+					)
+				) as decimal(10,2)) as valor_total_agua,
+				cast(sum(
+					if (idfaixa = 1, valor_esgoto,
+						if ($volume_cubico_consumido>faixa_final, ((faixa_final-faixa_inicial+1)*valor_esgoto), (($volume_cubico_consumido-faixa_inicial+1)*valor_esgoto))
+					) 
+				) as decimal(10,2)) as valor_total_esgoto 
+				FROM tbfaixaaguaesgoto
 
-	echo json_encode($result_json);
+				where faixa_inicial <= $volume_cubico_consumido";
+
+				
+	$stmt = DB::prepare($sql);
+	$stmt->execute();
+
+	$sel_row = $stmt->fetch(PDO::FETCH_ASSOC);
+	$total_agua = (double) $sel_row['valor_total_agua'];
+	$total_esgoto = (double) $sel_row['valor_total_esgoto'];
+
+	$obj = array("consumo" => $volume_cubico_consumido, "agua" => $total_agua, "esgoto" => $total_esgoto);
+
+	echo json_encode($obj);
 	
 
 ?>
